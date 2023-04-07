8a61e566dc01d239d9d9aa98737d41ae0ad95bcb
restauwoi <resta.design51@gmail.com>
2023-04-07 17:18:22 +0700

    store

---
+++ owner-dan-menu.js
@@ -0,0 +1,70 @@
+const fs = require('fs')
+
+global.namabot = "RestaBotz"
+global.namaowner = "Resta Store"
+global.footer_text = "© " + namabot
+global.pp_bot = fs.readFileSync("./image/foto.jpg")
+global.qris = fs.readFileSync("./image/qris.jpg")
+global.owner = ['6285743379513','6281328608503']
+global.sessionName = 'session'
+global.prefa = ['-_-']
+global.caption_pay = `Qris All Pay
+Ovo
+Dana
+Gopay
+
+Mau ganti payment? ketik .setpay
+`
+module.exports.helpMenu = (pushname) =>{
+  return `Halo ${pushname}
+
+*「 FITUR RESTA BOTZ 」*
+
+\`\`\`• !owner
+• !addsewa
+• !delsewa
+• !ceksewa
+• !listsewa
+• !pay
+• !setpay
+• !list
+• !addlist
+• !updatelist
+• !dellist
+• !jeda
+• !tambah
+• !kurang
+• !kali
+• !bagi
+• !setproses
+• !changeproses
+• !delsetproses
+• !setdone
+• !changedone
+• !delsetdone
+• !proses
+• !done
+• !welcome
+• !goodbye
+• !setwelcome
+• !changewelcome
+• !delsetwelcome
+• !setleft
+• !changeleft
+• !delsetleft
+• !antiwame
+• !antiwame2
+• !antilink
+• !antilink2
+• !open
+• !close
+• !hidetag
+\`\`\`
+
+📝 *NOTE*: 
+Fitur nya bisa dipakai dengan atau
+tanpa prefix (simbol awalan). Sebagai contoh 
+fitur .owner (prefix)
+dan bisa juga owner (tanpa prefix)
+`
+}
